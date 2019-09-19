1493ed566f290fded2e22ee5d6d6ed4d5cd60f07
akib22 <adnanakib75@gmail.com>
2019-09-19 17:20:44 +0600

    add 404 middleware

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const pageNotFound = require('./middlewares/404');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// page not found or 404 middleware
+app.use(pageNotFound);
 
 mongoose
   .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
