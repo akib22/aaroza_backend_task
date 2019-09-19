e2341a04550b5d10249b5f0dce505fba5dc186ac
akib22 <adnanakib75@gmail.com>
2019-09-19 17:05:31 +0600

    connect app with database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`app running on port ${process.env.PORT || 3000}`)
-);
+mongoose
+  .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () =>
+      console.log(`app running on port ${process.env.PORT || 3000}`)
+    );
+  })
+  .catch(err => console.log(err));
