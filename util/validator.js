ab6d99c675429b1a8595cc6cb50c48a5a232faf7
akib22 <adnanakib75@gmail.com>
2019-09-20 17:35:40 +0600

    fix some validtion bugs

--- util/validator.js
+++ util/validator.js
@@ -5,10 +5,10 @@ const signupInputValidation = data => {
   const err = [];
   let { username } = data;
   const { password } = data;
-  username = data.username.trim();
+  if (username) username = data.username.trim();
 
   // username validation
-  if (validator.isEmpty(username)) {
+  if (!username || validator.isEmpty(username)) {
     err.push('username is required.');
   } else if (!validator.isLength(username, { min: 3, max: 10 })) {
     err.push('username should be between 3 to 10 characters.');
@@ -27,16 +27,20 @@ const signupInputValidation = data => {
 // login input validation
 const loginInputValidation = data => {
   const err = [];
-  const { username, password } = data;
+  let { username } = data;
+  const { password } = data;
+  if (username) username = data.username.trim();
+
   // username validation
-  if (validator.isEmpty(username)) {
+  if (!username || validator.isEmpty(username)) {
     err.push('username is required.');
   }
   // password validation
   if (validator.isEmpty(password)) {
     err.push('password is required.');
   }
-  return err.length > 0 ? { err } : null;
+
+  return err.length > 0 ? { err } : { username, password };
 };
 
 module.exports = {
